9b3d57332f985ece719b8c8153912748df61bdad
zabooz <zabooz1988@gmail.com>
2023-01-15 16:24:29 +0100

    finished

---
+++ main.js
@@ -0,0 +1,30 @@
+const userInput = document.querySelector('#email');
+const emailRegex =/^[\w-\.]+@([\w-]+\.)+[\w-]+$/g;
+const notifyBtn = document.querySelector('button');
+const error = document.createElement('p');
+
+userInput.addEventListener('input', e => {
+    const width = window.innerWidth
+    if(width <= 600){
+        if(!emailRegex.test(userInput.value)){
+            error.textContent = 'Please provide a valid email address';
+            error.classList.add('error')
+            console.log(error.textContent)
+            userInput.after(error)
+        }else{
+            document.querySelector('.error').remove()
+        }
+    }
+});
+
+notifyBtn.addEventListener('click', e => {
+    console.log('hello world')
+    const form = document.querySelector('form')
+    if(!emailRegex.test(userInput.value)){
+        error.textContent = 'Please provide a valid email address';
+        error.classList.add('error')
+        console.log(error.textContent)
+        form.after(error)
+    }
+})
+
